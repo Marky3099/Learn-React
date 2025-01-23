2826baaa35228ee7f8821778b6d7527da4f95135
Marky3099 <markreym0@gmail.com>
2025-01-23 17:40:51 +0800

    Added Sample Recipe Data

--- recipeBook/src/components/Input.jsx
+++ recipeBook/src/components/Input.jsx
@@ -1,8 +1,14 @@
 import { useState } from "react";
+import RecipeCode from "./RecipeCode";
 
 export default function Input(){
 
     const [ingredients, setIngredients] = useState([]);
+    const [isShown, setIsShown] = useState(false);
+
+    function handleGetRecipe(){
+        setIsShown(!isShown);
+    }
 
     function handleSubmit(event){
         event.preventDefault();
@@ -27,19 +33,22 @@ export default function Input(){
                     {listIngredient}
                 </ul>
             </div>
-            <div className="ready">
-                <div className="ready-first-content">
-                    <div className="ready-first-title">
-                        <h3>Ready for a recipe?</h3>
-                    </div>
+            {listIngredient.length > 2 && 
+                <div className="ready">
                     <div className="ready-first-content">
-                        <p>Generate a recipe using AI</p>
+                        <div className="ready-first-title">
+                            <h3>Ready for a recipe?</h3>
+                        </div>
+                        <div className="ready-first-content">
+                            <p>Generate a recipe using AI</p>
+                        </div>
+                    </div>
+                    <div className="ready-second-content">
+                        <button onClick={handleGetRecipe}> Get a Recipe</button>
                     </div>
                 </div>
-                <div className="ready-second-content">
-                    <button> Get a Recipe</button>
-                </div>
-            </div>
+            }
+            {isShown && <RecipeCode />}
         </>
     )
 }
